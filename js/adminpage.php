f64f63f3269102e0fc9d2836653f8b9f5554b65e
Arabela Bonayon <142949121+ajbcute@users.noreply.github.com>
2023-12-21 22:58:34 +0800

    Add files via upload

---
+++ js/adminpage.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-xr+M1gdYXSSQ5/4iIcz7pOhUwL3/x3ovJk+BvLDUioLwYiYcJv1sSPD5oErI/NZ/MF1cfZH2/CMqDDxJu2I6vA==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>    <title>Client's Home Page </title>
+<style>
+/* Add your custom styles here */
+.container h1{
+    font-size: 50px;
+    font-family: 'Times New Roman', Times, serif ;
+    font-weight: bold;
+}
+</style>
+</head>
+<body>
+
+<div class="container">
+    <div class="row fixed-top bg-danger">
+        <div class="col-md-12"><!-- Navigation Bar -->
+            <ul class="nav nav-pills nav-fill">
+                <div class="btn-group">
+                    <button type="button" class="btn d-flex justify-content-center mt-2 ml-2" data-bs-toggle="dropdown" aria-expanded="false">                      
+                        <i class="fa-solid fa-list"></i>
+                    </button>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#sup-section">Support</a></li>
+                      <li><a class="dropdown-item" href="#leg-section">Legal</a></li>
+                    </ul>
+                </div>
+                <li class="nav-item">
+                    <a class="nav-link home text-white" aria-current="page" href="#">MANAGE PRODUCT</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link bundles text-white" href="#ORDERS-section">MANAGE ORDERS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link menu text-white" href="#USER-section">USERS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link contact text-white" href="#SALE-section">ON SALA</a>
+                </li>
+            </ul>
+        </div>
